9ce0a1f28f16af1346911707b995ae91b90079c6
Andrew Jeffery <dev@jeffas.net>
2024-04-03 08:24:51 +0100

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixpkgs-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    cargoNix = pkgs.callPackage ./Cargo.nix {};
+  in {
+    packages.${system}.lls = cargoNix.rootCrate.build;
+
+    formatter.${system} = pkgs.alejandra;
+
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+        pkgs.rustc
+        pkgs.cargo
+        pkgs.rustfmt
+        pkgs.clippy
+        pkgs.crate2nix
+      ];
+    };
+  };
+}
