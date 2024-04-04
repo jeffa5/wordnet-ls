876478f4e12b2719b71b5ee6237de548b30f754c
Andrew Jeffery <dev@jeffas.net>
2024-04-04 07:12:50 +0100

    Update vim file with lspconfig version

--- vim.lua
+++ vim.lua
@@ -1,9 +1,27 @@
-vim.lsp.start({
-  name = 'lls',
-  cmd = { 'target/release/lls' },
-  root_dir = '.',
+-- with lspconfig
+--
+require('lspconfig.configs').lls = {
+  default_config = {
+    cmd = { 'target/release/lls' },
+    filetypes = { 'text', 'markdown' },
+    root_dir = function(_)
+      return '/'
+    end,
+  },
+}
+require('lspconfig').lls.setup {
   init_options = { wordnet = os.getenv("WNSEARCHDIR") },
-})
+}
+
+-- or without lspconfig
+--
+-- vim.lsp.start({
+--   name = 'lls',
+--   cmd = { 'target/release/lls' },
+--   root_dir = '.',
+--   init_options = { wordnet = os.getenv("WNSEARCHDIR") },
+-- })
+
 vim.lsp.set_log_level("DEBUG")
 vim.keymap.set('n', 'K', vim.lsp.buf.hover, {noremap = true})
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {noremap = true})
